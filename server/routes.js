4ebf8994e8bb0f4af6f04b6120ab5a0fce0667a8
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-03-15 19:35:27 -0300

    feat: add routes

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,45 @@
+import { logger } from "./util.js";
+import config from "./config.js";
+import { Controller } from "./controller.js";
+
+const {
+  location,
+  pages: {
+    homeHTML
+  }
+} = config;
+
+const controller = new Controller();
+
+const routes = async (request, response) => {
+
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/") {
+
+    response.writeHead(302, {
+      "Location": location.home
+    })
+
+    return response.end();
+
+  }
+
+  if (method === "GET" && url === "/home") {
+
+    const {
+      stream
+    } = await controller.getFileStream(homeHTML);
+
+    return stream.pipe(response);
+
+  }
+
+}
 
 export function handler(request, response) {
-  return response.end("working fine!!!");
+
+  return routes(request, response)
+    .catch((error) => logger.error(`Deu ruimmmm: ${error.stack}`))
+
 }
